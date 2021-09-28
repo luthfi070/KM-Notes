4efd7895af620543d994a3ca51740cdd5377b155
luthfi070 <luthfimusafa@gmail.com>
2021-09-28 20:29:22 +0700

    added new DOM

---
+++ DOM/DOM-Create/index.js
@@ -0,0 +1,21 @@
+console.log("hello world");
+
+function writeComment() {
+  document.getElementById("comment-form-section").removeAttribute("class");
+}
+
+function submitComment() {
+  event.preventDefault();
+  let username = document.getElementById("input-username").value;
+  //   let comment = document.getElementById("input-commment").value;
+
+  console.log(document.getElementById("input-username").value);
+  console.log(document.getElementById("input-commment").value);
+}
+
+document
+  .getElementById("btn-submit")
+  .addEventListener("click", () => submitComment(event));
+document
+  .getElementById("btn-comment")
+  .addEventListener("click", () => writeComment());
